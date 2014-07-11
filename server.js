b6cf502152403a6219dc46261bb79e2245baf4c8
Ronan Drouglazet <ronan.drouglazet@ebuzzing.com>
2014-07-11 02:06:58 +0200

    begin interface, some bug appends

--- server.js
+++ server.js
@@ -17,10 +17,169 @@ GRebase.use("/", express.static(__dirname + "/static/"))
     res.send(404, "Not Found");
 });
 
-/*
- * @method exec execute a bash command and log it
- *
- */
+//socket io event for web interface
+socketIo.on("connection", function(socket) {
+    socket.emit("update", config);
+});
+
+//###############################################################
+
+var config;
+var STATUS = {
+    UNCHECKED: 1,
+    UP_TO_DATE: 2,
+    NEED_REBASE: 3,
+    REBASE_FAILED: 4
+}
+
+var init = function() {
+    fs.readFile("config.json", function(err, data) {
+        if (!err) {
+            createConfig(JSON.parse(data), 0);
+        } else {
+            console.error("GReabase error: please put config.json on GRebase root directory");
+        }
+    });
+};
+
+var startProcess = function() {
+    updateBranchList(function(err) {
+        if (!err) {
+            checkEachProject(0);
+        } else {
+            console.error(err);
+        }
+    });
+};
+
+var checkEachProject = function(current) {
+    if (current < config.length) {
+        checkEachBranch(config[current], 0, function() {
+            checkEachProject(current + 1);
+        });
+    } else {
+        startProcess();
+    }
+};
+
+var checkEachBranch = function(repo, current, done) {
+    if (current < repo.branch.length) {
+        socketIo.sockets.emit("update", config);
+        exec("repos/" + repo.name, "git checkout " + repo.branch[current].name, function(err, stdout, stderr) {
+            if (!err) {
+                getRebaseOrigin(repo, repo.branch[current].name, function(rebaseOrigin) {
+                    if (rebaseOrigin) {
+                        exec("repos/" + repo.name, "git pull --rebase && git reset --hard origin/" + repo.branch[current].name + " && git rebase origin/" + rebaseOrigin, function(err, stdout, stderr) {
+                            if (err) {
+                                console.log("REBASE AUTO FAILED FOR " + repo.branch[current].name);
+                                repo.branch[current].status = STATUS.REBASE_FAILED;
+                                exec("repos/" + repo.name, "git rebase --abort", function(err, stdout, stderr) {
+                                    checkEachBranch(repo, current + 1, done);
+                                });
+                            } else {
+                                if (stdout.endsWith("is up to date.\n")) {
+                                    console.log("UP TO DATE " + repo.branch[current].name);
+                                    repo.branch[current].status = STATUS.UP_TO_DATE;
+                                } else {
+                                    console.log("REBASE NEEDED FOR " + repo.branch[current].name);
+                                    repo.branch[current].status = STATUS.NEED_REBASE;
+                                }
+                                checkEachBranch(repo, current + 1, done);
+                            }
+                        });
+                    } else {
+                        console.log(repo.branch[current].name + " --> no rebase origin for this branch");
+                        repo.branch[current].status = STATUS.UNCHECKED;
+                        checkEachBranch(repo, current + 1, done);
+                    }
+                });
+
+            } else {
+                console.log(err);
+                checkEachBranch(repo, current + 1, done);
+            }
+        });
+    } else {
+        done();
+    }
+};
+
+var createConfig = function(json, current) {
+    if (current < json.length) {
+        fs.mkdir("repos", function(err) {
+            var repo = json[current];
+            var repoUrl = repo.url.replace(".git", "").split("/");
+
+            repo.name = repoUrl.pop();
+            repo.owner = repoUrl.pop();
+
+            fs.exists("repos/" + repo.name, function(exist) {
+                if (!exist) {
+                    console.log("clone repository " + repo.name + " ongoing...");
+                    console.log("This can take a short or very long time, depends of your repos size");
+                    gitCloneRepo("repos/", repo.url, function(err) {
+                        if (err) console.error("skip to the next repo", err);
+                        createConfig(json, current + 1);
+                    });
+                } else {
+                    console.warn(repo.name + " already exist, skip to the next repo");
+                    createConfig(json, current + 1);
+                }
+            });
+        });
+    } else {
+        config = json;
+        startProcess();
+    }
+};
+
+var gitCloneRepo = function(path, url, done) {
+    exec(path, "git clone " + url, function(err, stdout, stderr) {
+        done(err);
+    });
+};
+
+var getRebaseOrigin = function(repo, branchName, done) {
+    var rebaseOrigin = null;
+    repo.rebase.forEach(function(rules, index) {
+        var re = new RegExp(rules.to, "ig");
+        if (re.test(branchName)) {
+            rebaseOrigin = rules.from;
+        }
+    });
+    done(rebaseOrigin);
+};
+
+var updateBranchList = function(done) {
+    config.forEach(function(repo, index) {
+        exec("repos/" + repo.name, "git branch -r", function(err, stdout, stderr) {
+            if (!err && stdout) {
+                var aBranch = stdout.replace(/\s+origin\/HEAD.+/g, "").split("\n");
+                for (i = 0; i < aBranch.length; ++i) {
+                    if (aBranch[i] === "") {
+                        aBranch.splice(i--, 1);
+                    } else {
+                        aBranch[i] = aBranch[i].split("/")[1];
+                    }
+                }
+
+                config[index].branch = [];
+                aBranch.forEach(function(branchName, i) {
+                    config[index].branch.push({name: branchName, status: STATUS.UNCHECKED});
+                });
+            }
+            if (index === config.length - 1) {
+                done(err);
+            }
+        });
+    });
+};
+
+// little helpers
+String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
+
 var exec = function(localPath, command, done) {
     //console.log("#########################");
     //console.log("Exec command: ", command);
@@ -37,135 +196,7 @@ var exec = function(localPath, command, done) {
     });
 }
 
-var gitCloneRepo = function(path, url, done) {
-    exec(path, "git clone " + url, function(err, stdout, stderr) {
-        done(err);
-    });
-}
-
-var createConfig = function(config, current) {
-    if (current < config.length) {
-        fs.mkdir("repos", function(err) {
-            var repo = config[current];
-            var repoUrl = repo.url.replace(".git", "").split("/");
-
-            repo.name = repoUrl.pop();
-            repo.owner = repoUrl.pop();
-
-            fs.exists("repos/" + repo.name, function(exist) {
-                if (!exist) {
-                    console.log("clone repository " + repo.name + " ongoing..");
-                    gitCloneRepo("repos/", repo.url, function(err) {
-                        if (err) console.error("skip to the next repo", err);
-                        createConfig(config, current + 1);
-                    });
-                } else {
-                    console.warn(repo.name + " already exist, skip to the next repo");
-                    createConfig(config, current + 1);
-                }
-            });
-        });
-    } else {
-        startProcess(config);
-    }
-}
-
-var updateBranchList = function(config, done) {
-    config.forEach(function(repo, index) {
-        exec("repos/" + repo.name, "git branch -r", function(err, stdout, stderr) {
-            if (!err && stdout) {
-                var aBranch = stdout.replace(/\s+origin\/HEAD.+/g, "").split("\n");
-                for (i = 0; i < aBranch.length; ++i) {
-                    if (aBranch[i] === "") {
-                        aBranch.splice(i--, 1);
-                    } else {
-                        aBranch[i] = aBranch[i].split("/")[1];
-                    }
-                }
-                config[index].branch = aBranch;
-            }
-            if (index === config.length - 1) {
-                done(err);
-            }
-        });
-    });
-}
-
-var getRebaseOrigin = function(repo, branchName, done) {
-    var rebaseOrigin = null;
-    repo.rebase.forEach(function(rules, index) {
-        var re = new RegExp(rules.to, "ig");
-        if (re.test(branchName)) {
-            rebaseOrigin = rules.from;
-        }
-    });
-    done(rebaseOrigin);
-}
-
-var checkEachBranch = function(repo, current, done) {
-    if (current < repo.branch.length) {
-        exec("repos/" + repo.name, "git checkout " + repo.branch[current], function(err, stdout, stderr) {
-            if (!err) {
-                getRebaseOrigin(repo, repo.branch[current], function(rebaseOrigin) {
-                    if (rebaseOrigin) {
-                        exec("repos/" + repo.name, "git pull --rebase && git reset --hard origin/" + repo.branch[current] + " && git rebase origin/" + rebaseOrigin, function(err, stdout, stderr) {
-                            if (err) {
-                                console.log("MERGE FAILED FOR " + repo.branch[current]);
-                                exec("repos/" + repo.name, "git rebase --abort", function(err, stdout, stderr) {
-                                    checkEachBranch(repo, current + 1, done);
-                                });
-                            } else {
-                                console.log("MERGE WARN/OK FOR " + repo.branch[current]);
-                                checkEachBranch(repo, current + 1, done);
-                            }
-                        });
-                    } else {
-                        console.log(repo.branch[current] + " --> no rebase origin for this branch");
-                        checkEachBranch(repo, current + 1, done);
-                    }
-                });
-
-            } else {
-                console.log(err);
-                checkEachBranch(repo, current + 1, done);
-            }
-        });
-    } else {
-        done();
-    }
-}
-
-var checkEachProject = function(repos, current) {
-    if (current < repos.length) {
-        checkEachBranch(repos[current], 0, function() {
-            checkEachProject(repos, current + 1);
-        });
-    } else {
-        startProcess(repos);
-    }
-}
-
-var startProcess = function(config) {
-    updateBranchList(config, function(err) {
-        if (!err) {
-            checkEachProject(config, 0);
-        } else {
-            console.error(err);
-        }
-    });
-}
-
-var init = function() {
-    fs.readFile("config.json", function(err, data) {
-        if (!err) {
-            createConfig(JSON.parse(data), 0);
-        } else {
-            console.error("GReabase error: please put config.json on GRebase root directory");
-        }
-    });
-}
 
 init();
-
 serverIo.listen(process.env.PORT || 8080);
 console.info("now listening on port ", (process.env.PORT || 8080));
