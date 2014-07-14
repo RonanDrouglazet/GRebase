b82aac2d9840f411948535e4a1400272ba8a3941
RonanDrouglazet <ronan.drouglazet@ebuzzing.com>
2014-07-14 03:12:04 +0200

    add last commit author and rebase button tig status

--- static/interface.js
+++ static/interface.js
@@ -22,34 +22,68 @@ var updateUI = function(data) {
 
         oProject.branch.forEach(function(oBranch, i) {
             nProject = document.getElementById(oProject.name);
+
             var branchClassName = oProject.name + "_" + oBranch.name.replace(".", "");
             var branchExist = document.getElementsByClassName(branchClassName).length;
             var classColor = chooseColor(oBranch.status);
             var container = (oBranch.status === STATUS.ONGOING) ? "ongoing" : oProject.name;
+
+            oBranch.lastCommit = oBranch.lastCommit.replace("<", "(").replace(">", ")");
+
             if (nProject && !branchExist) {
                 $("#" + container + " ." + classColor).append(
                     "<div class='branch " + branchClassName + "'>" +
                         "<span class='label label-" + classColor + "'>" + classColor.toUpperCase() + "</span><span>" + oBranch.name + ((oBranch.parent !== "") ?  " (" + oBranch.parent + ")" : "") + "</span>" +
+                        "<span class='text-muted' style='float:right;font-size:11px'>" + oBranch.lastCommit + "</span>" +
+                        (oBranch.status === STATUS.NEED_REBASE && !oBranch.rebase ? "<span class='glyphicon glyphicon-refresh' data-toggle='tooltip' data-placement='right' title='rebase the branch'></span>" : "") +
                     "</div>"
                 );
+
+                if (oBranch.status === STATUS.NEED_REBASE) {
+                    $("." + branchClassName + " .glyphicon").tooltip();
+                    $("." + branchClassName + " .glyphicon").click(askRebase);
+                }
             } else if (nProject) {
-                var label = $("." + branchClassName + " span").get(0);
-                var branchName = $("." + branchClassName + " span").get(1);
                 var branch = $("." + branchClassName).get(0);
-                if (label) {
+                if (branch) {
+                    var label = $("." + branchClassName + " span").get(0);
+                    var branchName = $("." + branchClassName + " span").get(1);
+                    var lastCommit = $("." + branchClassName + " span").get(2);
+                    var rebaseButton = $("." + branchClassName + " span").get(3);
+
                     label.className = "label label-" + classColor;
                     label.innerHTML = classColor.toUpperCase();
                     branchName.innerHTML = oBranch.name;
+                    lastCommit.innerHTML = oBranch.lastCommit;
+
                     if (oBranch.parent !== "") {
                         branchName.innerHTML += " (" + oBranch.parent + ")";
                     }
+
                     if (branch.parentNode.className !== classColor || branch.parentNode.parentNode.parentNode.id !== container) {
                         $("#" + container + " ." + classColor).append(branch);
+                    }
+
+                    if (!rebaseButton && oBranch.status === STATUS.NEED_REBASE && !oBranch.rebase) {
+                        $(branch).append("<span class='glyphicon glyphicon-refresh' data-toggle='tooltip' data-placement='right' title='rebase the branch'></span>");
+                        $("." + branchClassName + " .glyphicon").tooltip();
+                        $("." + branchClassName + " .glyphicon").click(askRebase);
+                    } else if (rebaseButton && oBranch.status !== STATUS.NEED_REBASE) {
+                        $(rebaseButton).tooltip("destroy");
+                        $(rebaseButton).remove();
                     }
                 }
             }
         });
     });
+}
+
+var askRebase = function() {
+    var targetProject = this.parentNode.parentNode.parentNode.parentNode.id;
+    var targetBranch = this.parentNode.className.replace("branch ", "").replace(targetProject + "_", "");
+    socket.emit("rebase", {on: targetBranch, from:targetProject});
+    $(this).tooltip("destroy");
+    $(this).remove();
 }
 
 // choose a color / container from branch status
