b5fe33a28ff7969e833fab982b6ad8a681c3c647
Ronan Drouglazet <ronan.drouglazet@ebuzzing.com>
2014-10-08 01:55:44 +0200

    finish event and add queue

--- modules/grebase.js
+++ modules/grebase.js
@@ -27,21 +27,20 @@ exports.middleware = function(app, socketIo, express) {
     // static
     app.use("/", express.static(__dirname + "/../static/"));
 
-    // TODO REMOVE SOCKET.IO DONT NEDDED ANYMORE
     // socket io event for web interface
     sIo = socketIo;
     sIo.on("connection", function(socket) {
         socket.emit("update", config);
     });
 
-    genConfig(jsonConfig);
+    genConfigAndStartWatching(jsonConfig);
 
     return function(req, res, next) {
         next();
     };
 };
 
-var genConfig = function(json) {
+var genConfigAndStartWatching = function(json) {
     config = JSON.parse(JSON.stringify(json)); // clone object
     config.repository.forEach(function(repo, index) {
         var repoUrl = repo.url.replace(".git", "").split("/");
@@ -66,26 +65,51 @@ var startWatching = function(repoIndex) {
                 branchList.forEach(function(branch, index) {
                     aBranch.push(index);
                 });
-                updateBranchStatus(repoIndex, aBranch, 0, function() {
-                    gitApi.addEventOnRepo(repo.token, repo.owner, repo.name, gitApi.EVENTS.PUSH, onPushEvent);
-                    gitApi.addEventOnRepo(repo.token, repo.owner, repo.name, gitApi.EVENTS.CREATE, onCreateEvent);
-                    gitApi.addEventOnRepo(repo.token, repo.owner, repo.name, gitApi.EVENTS.DELETE, onDeleteEvent);
-                });
+
+                addToRepoQueue(repoIndex, aBranch);
+                gitApi.addEventOnRepo(repo.token, repo.owner, repo.name, gitApi.EVENTS.PUSH, onGitHubEvent);
+                gitApi.addEventOnRepo(repo.token, repo.owner, repo.name, gitApi.EVENTS.CREATE, onGitHubEvent);
             });
         });
     });
 };
 
-var onPushEvent = function(pushEvent) {
-    console.log("onPushEvent");
-};
+var addToRepoQueue = function(repoIndex, aBranch) {
+    var repo = config.repository[repoIndex];
 
-var onCreateEvent = function() {
-    console.log("onCreateEvent");
-};
+    if (!repo.queue) {
+        repo.queue = [];
+    }
 
-var onDeleteEvent = function() {
-    console.log("onDeleteEvent");
+    var checkQueue = function(queue) {
+        queue.shift();
+        if (queue.length > 0) {
+            repo.queue[0]();
+        } else {
+            updateInterface();
+        }
+    };
+
+    repo.queue.push(updateBranchStatus.bind(this, repoIndex, aBranch, 0, checkQueue.bind(this, repo.queue)));
+
+    if (repo.queue.length === 1) {
+        repo.queue[0]();
+    }
+}
+
+var onGitHubEvent = function(gitHubEvent) {
+    var repoName = gitHubEvent.repo.name.split("/")[1];
+    var repoIndex = getIndexFromName(config.repository, repoName);
+    var repo = config.repository[repoIndex];
+    var branchName = gitHubEvent.payload.ref.replace("refs/heads/", "");
+    var branchIndex;
+    var branchToUpdate;
+
+    updateBranchList(repo, function(branchList) {
+        branchIndex = getIndexFromName(repo.branch, branchName);
+        branchToUpdate = [branchIndex].concat(getRelatedBranchIndex(repo, branchName));
+        addToRepoQueue(repoIndex, branchToUpdate);
+    });
 };
 
 var getToken = function(req, res) {
@@ -187,91 +211,93 @@ var updateBranchStatus = function(repoIndex, aBranchIndex, current, done) {
         var next = updateBranchStatus.bind(this, repoIndex, aBranchIndex, current + 1, done);
         var repo = config.repository[repoIndex];
         var branch = repo.branch[aBranchIndex[current]];
+        var rebaseOrigin = getRebaseOrigin(repo, branch.name); // get the rebase origin from config rules for this branch
+
         branch.status = STATUS.ONGOING;
         updateInterface();
 
-        // get the rebase origin from config rules for this branch
-        getRebaseOrigin(repo, branch.name, function(rebaseOrigin) {
-            if (rebaseOrigin) {
-                branch.parent = rebaseOrigin;
-                // checkout on branch
-                gitCli.checkout(repo.name, branch.name, function() {
-                    // if a recover asked on this branch, reset it from backup and push on origin
-                    if (branch.recover.token) {
-                        gitCli.recover(repo, branch.name, branch.recover.token, function() {
-                            branch.status = STATUS.UNCHECKED;
-                            branch.recover.token = null;
-                            // re check the current branch after the recover
-                            updateBranchStatus(repoIndex, aBranchIndex, current, done);
-                        });
-                    } else {
-                        gitCli.reset(repo.name, branch.name, function() {
-                            gitCli.pull(repo.name, function() {
-                                gitApi.getCommit(repo.token, repo.owner, repo.name, branch.sha, function(error, data) {
-                                    branch.lastCommit = data.commit.author.email;
-                                    // get number of missing commits
-                                    gitCli.getMissingCommits(repo.name, branch.name, rebaseOrigin, function(missingCommits) {
-                                        branch.missCommit = missingCommits;
+        if (rebaseOrigin) {
+            branch.parent = rebaseOrigin;
+            // checkout on branch
+            gitCli.checkout(repo.name, branch.name, function() {
+                // if a recover asked on this branch, reset it from backup and push on origin
+                if (branch.recover.token) {
+                    gitCli.recover(repo, branch.name, branch.recover.token, function() {
+                        branch.status = STATUS.UNCHECKED;
+                        branch.recover.token = null;
+                        // re check the current branch after the recover
+                        updateBranchStatus(repoIndex, aBranchIndex, current, done);
+                    });
+                } else {
+                    gitCli.reset(repo.name, branch.name, function() {
+                        gitCli.pull(repo.name, function() {
+                            gitApi.getCommit(repo.token, repo.owner, repo.name, branch.sha, function(error, data) {
+                                if (data.commit) {
+                                    branch.lastCommit = data.commit.author.name;
+                                }
 
-                                        gitCli.merge(repo.name, rebaseOrigin, function(error, upToDate) {
-                                            if (error) {
-                                                branch.status = STATUS.REBASE_FAILED;
+                                // get number of missing commits
+                                gitCli.getMissingCommits(repo.name, branch.name, rebaseOrigin, function(missingCommits) {
+                                    branch.missCommit = missingCommits;
+
+                                    gitCli.merge(repo.name, rebaseOrigin, function(error, upToDate) {
+                                        if (error) {
+                                            branch.status = STATUS.REBASE_FAILED;
+                                            branch.merge.allow = branch.rebase.allow = false;
+                                            gitCli.reset(repo.name, branch.name, function() {
+                                                next();
+                                            });
+                                        } else {
+                                            if (upToDate) {
+                                                branch.status = STATUS.UP_TO_DATE;
                                                 branch.merge.allow = branch.rebase.allow = false;
-                                                gitCli.reset(repo.name, branch.name, function() {
-                                                    next();
-                                                });
+                                                next();
                                             } else {
-                                                if (upToDate) {
-                                                    branch.status = STATUS.UP_TO_DATE;
-                                                    branch.merge.allow = branch.rebase.allow = false;
-                                                    next();
+                                                branch.status = STATUS.NEED_REBASE;
+                                                branch.merge.allow = true;
+
+                                                // if we want a merge, so do it and push
+                                                if (branch.merge.token) {
+                                                    mergeAndPush(repo, branch.name, rebaseOrigin, branch.merge.token, function() {
+                                                        branch.status = STATUS.UP_TO_DATE;
+                                                        branch.backup = config.backup[repo.url][branch.name] = new Date().toLocaleString();
+                                                        writeBackup(config.backup);
+                                                        branch.merge.token = null;
+                                                        next();
+                                                    });
                                                 } else {
-                                                    branch.status = STATUS.NEED_REBASE;
-                                                    branch.merge.allow = true;
+                                                    // else try to rebase to know if allow or not
+                                                    gitCli.reset(repo.name, branch.name, function() {
+                                                        gitCli.rebase(repo.name, rebaseOrigin, function(error, upToDate) {
+                                                            branch.rebase.allow = !error;
 
-                                                    // if we want a merge, so do it and push
-                                                    if (branch.merge.token) {
-                                                        mergeAndPush(repo, branch.name, rebaseOrigin, branch.merge.token, function() {
-                                                            branch.status = STATUS.UP_TO_DATE;
-                                                            branch.backup = config.backup[repo.url][branch.name] = new Date().toLocaleString();
-                                                            writeBackup(config.backup);
-                                                            branch.merge.token = null;
-                                                            next();
-                                                        });
-                                                    } else {
-                                                        // else try to rebase to know if allow or not
-                                                        gitCli.reset(repo.name, branch.name, function() {
-                                                            gitCli.rebase(repo.name, rebaseOrigin, function(error, upToDate) {
-                                                                branch.rebase.allow = !error;
-
-                                                                if (!error && branch.rebase.token) {
-                                                                    rebaseAndPush(repo, branch.name, rebaseOrigin, branch.rebase.token, function() {
-                                                                        branch.status = STATUS.UP_TO_DATE;
-                                                                        branch.backup = config.backup[repo.url][branch.name] = new Date().toLocaleString();
-                                                                        writeBackup(config.backup);
-                                                                        branch.rebase.token = null;
-                                                                        next();
-                                                                    });
-                                                                } else {
+                                                            if (!error && branch.rebase.token) {
+                                                                rebaseAndPush(repo, branch.name, rebaseOrigin, branch.rebase.token, function() {
+                                                                    branch.status = STATUS.UP_TO_DATE;
+                                                                    branch.backup = config.backup[repo.url][branch.name] = new Date().toLocaleString();
+                                                                    writeBackup(config.backup);
+                                                                    branch.rebase.token = null;
                                                                     next();
-                                                                }
-                                                            });
+                                                                });
+                                                            } else {
+                                                                next();
+                                                            }
                                                         });
-                                                    }
+                                                    });
                                                 }
                                             }
-                                        });
+                                        }
                                     });
                                 });
                             });
                         });
-                    }
-                });
-            } else {
-                branch.status = STATUS.UNCHECKED;
-                next();
-            }
-        });
+                    });
+                }
+            });
+        } else {
+            branch.status = STATUS.UNCHECKED;
+            next();
+        }
     } else {
         done();
     }
@@ -327,7 +353,7 @@ var writeBackup = function(backup, done) {
 };
 
 // get rebase origin for a branch from rules defined on config.json
-var getRebaseOrigin = function(repo, branchName, done) {
+var getRebaseOrigin = function(repo, branchName) {
     var rebaseOrigin = null;
     repo.rebase.forEach(function(rules, index) {
         var re = new RegExp(rules.to, "ig");
@@ -335,7 +361,24 @@ var getRebaseOrigin = function(repo, branchName, done) {
             rebaseOrigin = rules.from;
         }
     });
-    done(rebaseOrigin);
+    return rebaseOrigin;
+};
+
+var getRelatedBranchIndex = function(repo, branchName) {
+    var relatedBranchIndex = [];
+    repo.rebase.forEach(function(rules, index) {
+        var reFrom = new RegExp(rules.from, "ig");
+        var reTo = null;
+        if (reFrom.test(branchName)) {
+            repo.branch.forEach(function(branch, index) {
+                reTo = new RegExp(rules.to, "ig");
+                if (reTo.test(branch.name)) {
+                    relatedBranchIndex.push(index);
+                }
+            });
+        }
+    });
+    return relatedBranchIndex;
 };
 
 var rebaseAndPush = function(repo, branchName, rebaseOrigin, tokenToPush, done) {
@@ -371,13 +414,12 @@ var mergeAndPush = function(repo, branchName, rebaseOrigin, tokenToPush, done) {
 var ask = function(req, res) {
     gitApi.oauth(req, res, function(token) {
         var repoIndex = getIndexFromName(config.repository, req.param("repo"));
-        var branchIndex = getIndexFromName(config.repository[repoIndex].branch, req.param("branch"));
-        var branch = config.repository[repoIndex].branch[branchIndex];
-        branch[req.param("type")].token = token; // active action by filling token to push
+        var repo = config.repository[repoIndex];
+        var branchIndex = getIndexFromName(repo.branch, req.param("branch"));
+        var branch = repo.branch[branchIndex];
 
-        updateBranchStatus(repoIndex, [branchIndex], 0, function() {
-            updateInterface();
-        });
+        branch[req.param("type")].token = token; // active action by filling token to push
+        addToRepoQueue(repoIndex, [branchIndex]);
 
         res.write("<script>window.close()</script>");
         res.send();
